0011f110b8a33ff6729e02f05bd3a97a00abb484
Paddym1991 <paddym1991.pm@gmail.com>
2017-05-13 17:14:58 +0100

    more reviews

--- app/controllers/Dashboard.java
+++ app/controllers/Dashboard.java
@@ -8,18 +8,26 @@ import java.util.List;
 
 public class Dashboard extends Controller
 {
-  public static void index()
-  {
-    Logger.info("Rendering Dashboard");
-    List<Todo> todolist = Todo.findAll();
-    render("dashboard.html", todolist);
-  }
+    public static void index()
+    {
+        Logger.info("Rendering Dashboard");
+        List<Todo> todolist = Todo.findAll();
+        render("dashboard.html", todolist);
+    }
 
-  public static void addTodo(String title)
-  {
-    Todo todo = new Todo(title);
-    todo.save();
-    Logger.info("Adding Todo" + title);
-    redirect("/dashboard");
-  }
+    public static void addTodo(String title)
+    {
+        Todo todo = new Todo(title);
+        todo.save();
+        Logger.info("Adding Todo" + title);
+        redirect("/dashboard");
+    }
+
+    public static void deleteTodo(Long id)
+    {
+        Todo todo = Todo.findById(id);
+        todo.delete();
+        Logger.info("Deleting " + todo.title);
+        redirect("/dashboard");
+    }
 }
